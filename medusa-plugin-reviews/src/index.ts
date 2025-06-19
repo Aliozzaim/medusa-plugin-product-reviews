c6b706983ee6c59db1e1570f91648ff5056ce015
Ali Ozzaim <125793435+Aliozzaim@users.noreply.github.com>
2025-06-19 22:55:29 +0200

    feat: export ProductReviewService, ProductReview, and ProductReviewsPage from index

--- medusa-plugin-reviews/src/index.ts
+++ medusa-plugin-reviews/src/index.ts
@@ -1,3 +1,7 @@
 import loader from "./loaders/product-review-loader";
+import ProductReviewService from "./services/product-review";
+import { ProductReview } from "./models/product-review";
+import ProductReviewsPage from "./admin/routes/reviews/page";
 
 export default loader;
+export { ProductReviewService, ProductReview, ProductReviewsPage };
